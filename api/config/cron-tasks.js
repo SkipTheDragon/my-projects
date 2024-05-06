dd6f436032d40e41ea07b88bf7b24167b189c170
Tudorache Leonard Valentin <valileo2000@gmail.com>
2024-05-06 23:49:59 +0300

    fix: added cron task

--- api/config/cron-tasks.js
+++ api/config/cron-tasks.js
@@ -4,13 +4,13 @@ module.exports = {
    * Every monday at 1am.
    */
 
-  // githubSyncReposForUserCron: {
-  //   task: async ({strapi}) => {
-  //     await strapi.service('api::repository.repository').syncWithGithub('SkipTheDragon');
-  //     // Add your own logic here (e.g. send a queue of email, create a database backup, etc.).
-  //   },
-  //   options: {
-  //     rule: "* * * * *",
-  //   },
-  // },
+  githubSyncReposForUserCron: {
+    task: async ({strapi}) => {
+      await strapi.service('api::repository.repository').syncWithGithub('SkipTheDragon');
+      // Add your own logic here (e.g. send a queue of email, create a database backup, etc.).
+    },
+    options: {
+      rule: "0 0 * * *",
+    },
+  },
 };
