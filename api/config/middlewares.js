c2b5de8838683939339174ab530ee9f744f69188
Tudorache Leonard Valentin <valileo2000@gmail.com>
2024-05-09 20:15:43 +0300

    fix: cors + update strapi

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,8 +1,15 @@
+console.log(process.env.CORS_ALLOW_ORIGIN, ' uirl')
 module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      header: '*',
+      origin: process.env.CORS_ALLOW_ORIGIN.split(',')
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
